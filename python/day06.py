82b3f646e40354e0fbaf6d7d0836e538ff421ccf
Brian Perrett <perrettbrian@gmail.com>
2023-12-06 20:54:50 -0800

    binary search

--- python/day06.py
+++ python/day06.py
@@ -2,8 +2,38 @@ import math
 data = """Time:        40     81     77     72
 Distance:   219   1012   1365   1089"""
 
-def find_wins(best_time: int, distance: int):
-    return sum(is_win(best_time, distance, i) for i in range(best_time))
+def count_wins(best_time: int, distance: int):
+    return find_last(best_time, distance) - find_first(best_time, distance) + 1
+
+def find_first(best_time: int, distance: int):
+    check = 1
+    step = best_time // 2
+    while True:
+        check_win = is_win(best_time, distance, check)
+        plus_one = is_win(best_time, distance, check + 1)
+        if not check_win and plus_one:
+            return check + 1
+
+        step = math.ceil(step / 2)
+        if check_win:
+            check -= step
+        else:
+            check += step
+
+def find_last(best_time: int, distance: int):
+    check = best_time
+    step = best_time // 2
+    while True:
+        check_win = is_win(best_time, distance, check)
+        minus_one = is_win(best_time, distance, check - 1)
+        if not check_win and minus_one:
+            return check - 1
+
+        step = math.ceil(step / 2)
+        if check_win:
+            check += step
+        else:
+            check -= step
 
 def is_win(best_time: int, distance: int, wind_up: int):
     return (best_time - wind_up) * wind_up > distance
@@ -16,8 +46,6 @@ def parse(data):
 
 if __name__ == '__main__':
     times, distances = parse(data)
-    print(math.prod(find_wins(time, dist) for time, dist in zip(times, distances)))
-
+    print(math.prod(count_wins(time, dist) for time, dist in zip(times, distances)))
     time, dist = int(''.join(map(str, times))), int(''.join(map(str, distances)))
-    x = find_wins(time, dist)
-    print(x)
+    print(count_wins(time, dist))
