6a885d4d466802bd9d9ce0763cebde2a82a78a8c
Brian Perrett <perrettbrian@gmail.com>
2023-12-08 22:04:07 -0800

    shorter

--- python/day09.py
+++ python/day09.py
@@ -4,22 +4,13 @@ data = (Path(__file__).parent.parent / "data" / "day09.txt").read_text()
 
 
 def predict_next(values: list[int]):
-    values = values.copy()
     diff = [b - a for a, b in zip(values, values[1:])]
-    if all(x == 0 for x in diff):
-        diff.append(0)
-        val = 0
-    else:
-        val = predict_next(diff)[-1]
-    values.append(values[-1] + val)
-    return values
-
-def parse(data):
-    return [list(map(int, line.split())) for line in data.splitlines()]
+    val = 0 if all(x == 0 for x in diff) else predict_next(diff)[-1]
+    return values + [values[-1] + val]
 
 
 if __name__ == '__main__':
-    items = parse(data)
+    items = [list(map(int, line.split())) for line in data.splitlines()]
 
     print(sum(predict_next(item)[-1] for item in items))
     print(sum(predict_next(item[::-1])[-1] for item in items))
