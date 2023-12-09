8942bc64b1f691cc1b0438f3487f3776c3b7d411
Brian Perrett <perrettbrian@gmail.com>
2023-12-08 22:00:05 -0800

    You can just reverse the list

--- python/day09.py
+++ python/day09.py
@@ -1,39 +1,25 @@
 from pathlib import Path
-from dataclasses import dataclass
 
 data = (Path(__file__).parent.parent / "data" / "day09.txt").read_text()
 
 
-@dataclass
-class History:
-    history: list[int]
-
-    def predict_next(self, values: list[int]):
-        values = values.copy()
-        diff = [b - a for a, b in zip(values, values[1:])]
-        if all(x == 0 for x in diff):
-            diff.append(0)
-            val = 0
-        else:
-            val = self.predict_next(diff)[-1]
-        values.append(values[-1] + val)
-        return values
-
-    def predict_previous(self, values: list[int]):
-        diff = [b - a for a, b in zip(values, values[1:])]
-        if all(x == 0 for x in diff):
-            val = 0
-        else:
-            val = self.predict_previous(diff)[0]
-        values = [values[0] - val] + values
-        return values
+def predict_next(values: list[int]):
+    values = values.copy()
+    diff = [b - a for a, b in zip(values, values[1:])]
+    if all(x == 0 for x in diff):
+        diff.append(0)
+        val = 0
+    else:
+        val = predict_next(diff)[-1]
+    values.append(values[-1] + val)
+    return values
 
 def parse(data):
-    return [History(list(map(int, line.split()))) for line in data.splitlines()]
+    return [list(map(int, line.split())) for line in data.splitlines()]
 
 
 if __name__ == '__main__':
     items = parse(data)
 
-    print(sum(item.predict_next(item.history)[-1] for item in items))
-    print(sum(item.predict_previous(item.history)[0] for item in items))
+    print(sum(predict_next(item)[-1] for item in items))
+    print(sum(predict_next(item[::-1])[-1] for item in items))
