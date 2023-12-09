f98d4025e7e36b673ea0279bea7978acd7ac3b2e
Brian Perrett <perrettbrian@gmail.com>
2023-12-08 21:47:51 -0800

    Slightly better

--- python/day09.py
+++ python/day09.py
@@ -12,46 +12,28 @@ class History:
         values = values.copy()
         diff = [b - a for a, b in zip(values, values[1:])]
         if all(x == 0 for x in diff):
-            diff.append(diff[-1])
+            diff.append(0)
+            val = 0
         else:
-            diff.append(self.predict_next(diff)[-1])
-        values.append(values[-1] + diff[-1])
+            val = self.predict_next(diff)[-1]
+        values.append(values[-1] + val)
         return values
 
     def predict_previous(self, values: list[int]):
-        values = values.copy()
         diff = [b - a for a, b in zip(values, values[1:])]
         if all(x == 0 for x in diff):
-            diff.append(0)
+            val = 0
         else:
-            diff.append(self.predict_next(diff)[-1])
-            diff = [self.predict_previous(diff)[0]] + diff
-        values.append(values[-1] + diff[-1])
-        values = [values[0] - diff[0]] + values
+            val = self.predict_previous(diff)[0]
+        values = [values[0] - val] + values
         return values
 
 def parse(data):
-    x: list[History] = []
-    for line in data.splitlines():
-        line = list(map(int, line.split()))
-        x.append(
-            History(history=line)
-        )
-    return x
+    return [History(list(map(int, line.split()))) for line in data.splitlines()]
 
 
 if __name__ == '__main__':
     items = parse(data)
 
-    total = 0
-    for item in items:
-        values = item.predict_next(item.history)
-        total += values[-1]
-    print(total)
-
-
-    total = 0
-    for item in items:
-        values = item.predict_previous(item.history)
-        total += values[0]
-    print(total)
+    print(sum(item.predict_next(item.history)[-1] for item in items))
+    print(sum(item.predict_previous(item.history)[0] for item in items))
